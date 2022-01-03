eb6cfa869a0e21ed5a84e542cf2af2b5c5dbfa9f
lucasnahuelacuna <lucas.nahuel.acuna@gmail.com>
2022-01-03 17:01:29 -0300

    Write and deploy smart contract in the ethereum ropsten test network

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x5c3a8f39ea47db7f0AdD3fCDcB34f29144FD42e7';
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,13 @@
+//https://eth-ropsten.alchemyapi.io/v2/Qoa09UdfbyvEOAWFDhH7-TwgTlzbVj0F
+
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/Qoa09UdfbyvEOAWFDhH7-TwgTlzbVj0F',
+      accounts: [ 'cdf0df7a6dc3b476576b17d374e05eaa37b313e7fbe18536bf56b5291e5ef8f9' ]
+    }
+  }
 };
