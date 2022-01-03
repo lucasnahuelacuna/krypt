c7a62477ceb64bffe2de4084222df4e9f3f4d7bc
lucasnahuelacuna <lucas.nahuel.acuna@gmail.com>
2022-01-03 20:02:28 -0300

    Connecting to metamask wallet

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -19,8 +19,44 @@ const getEthereumContract = () => {
 }
 
 export const TransactionProvider = ({ children }) => {
+    const [currentAccount, setCurrentAccount] = useState('')
+
+    const checkIfWalletIsConnected = async () => {
+        try {
+            if(!ethereum) return alert("Please install metamask")
+
+            const accounts = await ethereum.request({ method: "eth_accounts" })
+
+            if(accounts.length) {
+                setCurrentAccount(accounts[0])
+            } else {
+                console.log('No accounts found')
+            }
+        } catch (error) {
+            console.log(error)
+            throw new Error("No ethereum object")
+        }
+    }
+
+    const connectWallet = async () => {
+        try {
+          if (!ethereum) return alert("Please install MetaMask")
+    
+          const accounts = await ethereum.request({ method: "eth_requestAccounts" })
+    
+          setCurrentAccount(accounts[0])
+        } catch (error) {
+          console.log(error)
+          throw new Error("No ethereum object")
+        }
+    }
+
+    useEffect(() => {
+        checkIfWalletIsConnected()
+    }, [])
+
     return (
-        <TransactionContext.Provider value={{ value: 'test' }}>
+        <TransactionContext.Provider value={{ connectWallet }}>
             { children }
         </TransactionContext.Provider>
     )
